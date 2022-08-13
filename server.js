b10ac0f0c2599c2b2f61741fe4fff2bbe2352d61
Namencanto <mateusz.ordon22@gmail.com>
2022-08-13 18:48:05 +0200

    heroku configuration

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.get("/offert", function (req, res) {
 //   res.render("pages/about");
 // });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log("Server is listening on port 8080");
