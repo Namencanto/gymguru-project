7a32b1088aa9112ce5812a6ebb78de07fa4fae1a
Namencanto <mateusz.ordon22@gmail.com>
2022-08-25 19:19:05 +0200

    test 4

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -2,37 +2,37 @@ module.exports = {
   db: process.env.MONGODB_URI || "mongodb://localhost:27017/test2",
 };
 
-const express = require("express");
-const app = express();
-const path = require("path");
-const router = express.Router();
-const ejs = require("ejs");
-// set the view engine to ejs
-app.set("view engine", "ejs");
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const router = express.Router();
+// const ejs = require("ejs");
+// // set the view engine to ejs
+// app.set("view engine", "ejs");
 
-// use res.render to load up an ejs view file
-app.use(express.static(path.join(__dirname, "public")));
+// // use res.render to load up an ejs view file
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.render(path.join(__dirname, "views/pages/index.ejs"), {
-    title: "your best gym",
-  });
-});
-
-app.get("/about", function (req, res) {
-  res.render(path.join(__dirname, "views/pages/about.ejs"), {
-    title: "about us",
-  });
-});
-app.get("/offert", function (req, res) {
-  res.render(path.join(__dirname, "views/pages/gym-offert.ejs"), {
-    title: "our offert",
-  });
-});
-// about page
-// app.get("/about", function (req, res) {
-//   res.render("pages/about");
+// app.get("/", function (req, res) {
+//   res.render(path.join(__dirname, "views/pages/index.ejs"), {
+//     title: "your best gym",
+//   });
 // });
 
-app.listen(process.env.PORT || 8080);
-console.log("Server is listening on port 8080");
+// app.get("/about", function (req, res) {
+//   res.render(path.join(__dirname, "views/pages/about.ejs"), {
+//     title: "about us",
+//   });
+// });
+// app.get("/offert", function (req, res) {
+//   res.render(path.join(__dirname, "views/pages/gym-offert.ejs"), {
+//     title: "our offert",
+//   });
+// });
+// // about page
+// // app.get("/about", function (req, res) {
+// //   res.render("pages/about");
+// // });
+
+// app.listen(process.env.PORT || 8080);
+// console.log("Server is listening on port 8080");
