47b2d4ee0fe43dd1854774a096c72bd13dad5aa4
Namencanto <mateusz.ordon22@gmail.com>
2022-08-25 19:14:47 +0200

    test 2

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = express.Router();
+const mongoose = require("mongoose");
+const dbConfig = require("./backend/config/db.config.js");
+const auth = require("./backend/middlewares/auth.js");
+const errors = require("./backend/middlewares/errors.js");
+const unless = require("express-unless");
 const ejs = require("ejs");
+
+const userData = require("./Backend/controllers/users.controller");
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-// use res.render to load up an ejs view file
+mongoose
+  .connect(dbConfig.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connected");
+    },
+    (error) => {
+      console.log("Database can't be connected: " + error);
+    }
+  );
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// initialize routes
+app.use("/users", require("./Backend/routes/users.routes.js"));
+
+// middleware for error responses
+app.use(errors.errorHandler);
+
+// app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
@@ -14,7 +47,6 @@ app.get("/", function (req, res) {
     title: "your best gym",
   });
 });
-
 app.get("/about", function (req, res) {
   res.render(path.join(__dirname, "views/pages/about.ejs"), {
     title: "about us",
@@ -25,10 +57,39 @@ app.get("/offert", function (req, res) {
     title: "our offert",
   });
 });
-// about page
-// app.get("/about", function (req, res) {
-//   res.render("pages/about");
-// });
+app.get("/account", function (req, res) {
+  // set jwt in cookie for auth user
+  if (!req.cookies.jwt) {
+    let options = {
+      maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+      httpOnly: true, // The cookie only accessible by the web server
+      signed: false, // Indicates if the cookie should be signed
+    };
+    // Set cookie
+    res.cookie("jwt", userData.userData.token, options); // options is optional
+  }
+  // render page
+  res.render(path.join(__dirname, "views/pages/account.ejs"), {
+    title: "our offert",
+    userData: userData.userData,
+  });
+});
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(process.env.PORT || 8080);
 console.log("Server is listening on port 8080");
