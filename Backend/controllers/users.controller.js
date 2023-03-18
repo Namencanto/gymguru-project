9398a3a96dda2a13ffbb9d781a7a11eff5487121
Namencanto <mateusz.ordon22@gmail.com>
2023-03-18 14:38:27 +0100

    fixed buttons style and deleted css folder from repo

--- Backend/controllers/users.controller.js
+++ Backend/controllers/users.controller.js
@@ -2,11 +2,6 @@ const bcrypt = require("bcryptjs");
 const userServices = require("../services/users.services.js");
 const User = require("../models/user.model");
 
-/**
- * 1. To secure the password, we are using the bcryptjs, It stores the hashed password in the database.
- * 2. In the SignIn API, we are checking whether the assigned and retrieved passwords are the same or not using the bcrypt.compare() method.
- * 3. In the SignIn API, we set the JWT token expiration time. Token will be expired within the defined duration.
- */
 exports.register = (req, res, next) => {
   const { password } = req.body;
 
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -49,11 +49,6 @@ const UserSchema = new Schema({
   },
 });
 
-/**
- *  Here we are creating and setting an id property and 
-    removing _id, __v, and the password hash which we do not need 
-    to send back to the client.
- */
 UserSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
@@ -64,10 +59,6 @@ UserSchema.set("toJSON", {
   },
 });
 
-/**
- * 1. The userSchema.plugin(uniqueValidator) method won’t let duplicate email id to be stored in the database.
- * 2. The unique: true property in email schema does the internal optimization to enhance the performance.
- */
 UserSchema.plugin(uniqueValidator, { message: "Email already in use." });
 
 const User = mongoose.model("user", UserSchema);
